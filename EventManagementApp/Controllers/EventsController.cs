6846093ac5c229bfa855ded885716f003cfc3072
Snehal <snehal.mankar@exceederinfotech.com>
2025-03-21 18:55:28 +0530

    Web API integration in ASP.Net MVC app

---
+++ EventManagementApp/Controllers/EventsController.cs
@@ -0,0 +1,124 @@
+﻿using EventManagement.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace EventManagementApp.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly HttpClient _client;
+        private readonly IConfiguration _config;
+        public EventsController(IConfiguration configuration)
+        {
+            _config = configuration;
+
+            _client = new HttpClient();
+            string baseAdd = _config.GetValue<string>("Settings:baseAddress");
+            Uri baseAddress = new Uri(baseAdd);
+            _client.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<EventDetailsDTO> EventList = new List<EventDetailsDTO>();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "EventSchedule/GetAllEventDetails").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string Data = response.Content.ReadAsStringAsync().Result;
+                EventList = JsonConvert.DeserializeObject<List<EventDetailsDTO>>(Data);
+            }
+            return View(EventList);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateEventDTO EventDTO)
+        {
+            try
+            {
+                string data = JsonConvert.SerializeObject(EventDTO);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = _client.PostAsync(_client.BaseAddress + "EventSchedule/SaveEvent", content).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    TempData["SuccessMessage"] = "Data save successfully!";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                throw;
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EventDetails(int id)
+        {
+            List<EventDetailsDTO> EventList = new List<EventDetailsDTO>();
+            EventDetailsDTO eventDetailsDTO = new EventDetailsDTO();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "EventSchedule/GetEventDetailsByEventID/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                //EventList = JsonConvert.DeserializeObject<List<EventDetailsDTO>>(data);
+                eventDetailsDTO = JsonConvert.DeserializeObject<EventDetailsDTO>(data);
+            }
+
+            List<EventScheduleDTO> eventSchedule = new List<EventScheduleDTO>();
+            CreateEventDTO createEvent = new CreateEventDTO();
+            HttpResponseMessage responsen = _client.GetAsync(_client.BaseAddress + "EventSchedule/GetScheduleDetailsByEventID/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = responsen.Content.ReadAsStringAsync().Result;
+                eventSchedule = JsonConvert.DeserializeObject<List<EventScheduleDTO>>(data);
+            }
+            eventDetailsDTO.EventSchedule = eventSchedule.ToList();
+            return View(eventDetailsDTO);
+        }
+
+        public async Task<IActionResult> DeleteEvent(int id)
+        {
+            try
+            {
+                HttpResponseMessage response = _client.DeleteAsync(_client.BaseAddress + "EventSchedule/DeleteEventByEventID/"
+                                                + id).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                return View();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public IActionResult Errorpage()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DisplayEventDetails(int id)
+        {
+            List<EventScheduleDTO> EventList = new List<EventScheduleDTO>();
+            CreateEventDTO createEvent = new CreateEventDTO();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "EventSchedule/GetScheduleDetailsByEventID/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                EventList = JsonConvert.DeserializeObject<List<EventScheduleDTO>>(data);
+            }
+            return PartialView("DisplayEventDetails", EventList);
+        }
+    }
+}
---
+++ EventManagementApp/Models/EventScheduleDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace EventManagement.Model
+{
+    public class EventScheduleDTO
+    {
+        [DisplayName("Event name")]
+        public string? EventTitle { get; set; }
+
+        [DisplayName("Schedule date")]
+        public string? ScheduleDate { get; set; }
+
+        [DisplayName("Start time")]
+        public string? StartTime { get; set; }
+        [DisplayName("End time")]
+        public string? EndTime { get; set; }
+    }
+}
